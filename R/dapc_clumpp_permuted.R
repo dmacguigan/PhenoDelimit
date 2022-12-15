33500dd88796930699d63a7a99e09177b6aa94f4
dmacguigan <dmacguig@buffalo.edu>
2022-12-15 13:28:15 -0500

    modified permutation test protocol

--- R/dapc_clumpp_permuted.R
+++ R/dapc_clumpp_permuted.R
@@ -11,8 +11,6 @@ dapc_clumpp_permuted <- function(wd, data, n.groups, model.numbers, models, perc
       }
     }
   }
-  clumpp_permutated_results <- read_clumpp_results_perumtation(wd=wd, perc.var=perc.var, model.numbers=model.numbers, permutations=permutations)
-  return(clumpp_permutated_results)
 }
 
 
@@ -81,31 +79,4 @@ run_dapc_clumpp_permutation <- function(data, modelNumber, k, model, perc.var, s
   shell(paste("CLUMPP", f, sep=" "))
 
   close(file)
-}
-
-# read in CLUMPP results from miscfiles
-read_clumpp_results_perumtation <- function(wd, perc.var, model.numbers, permutations){
-  setwd(wd)
-  c1=numeric()
-  c2=numeric()
-  c3=numeric()
-  c4=numeric()
-  for(i in 1:length(perc.var)){
-    for(j in 1:length(model.numbers)){
-      for(p in 1:permutations){
-        fileName <- Sys.glob(paste("m", model.numbers[j], "_perVar-", perc.var[i], ".p", p, ".miscfile", sep=""))
-        temp <- grep("The highest value of H' is:", readLines(fileName), value=TRUE)
-        H <- as.numeric(strsplit(temp, " ")[[1]][7]) # get only H' number
-        c1 <- c(c1, model.numbers[j])
-        c2 <- c(c2, perc.var[i])
-        c3 <- c(c3, H)
-        c4 <- c(c4, p)
-      }
-    }
-  }
-  
-  # create data frame with results
-  H_df <- data.frame(model=c1, perc.pca=c2, H=c3, permutation=c4)
-  
-  return(H_df)
 }
